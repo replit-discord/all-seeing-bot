acd5eead7dc1554892251b8ef858b497f7b13999
eankeen <24364012+eankeen@users.noreply.github.com>
2019-12-07 23:31:30 -0800

    feat: add home dashboard home dashboard shows all guilds user is in and displays the user

--- website/controllers/dashboardController.js
+++ website/controllers/dashboardController.js
@@ -1,7 +1,16 @@
 export function homeController(req, res) {
- res.render("pages/dashboardHome");
+  res.render("pages/dashboardHome", {
+    id: req.user.id,
+    username: req.user.username,
+    avatar: req.user.avatar,
+    discriminator: req.user.discriminator,
+    mfa_enabled: req.user.mfa_enabled,
+    guilds: req.user.guilds
+  });
 }
 
 export function profileController(req, res) {
-  res.render("pages/dashboardProfile")
+  res.render("pages/dashboardProfile", {
+    username: req.user.username
+  })
 }
