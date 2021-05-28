4f914b65682474c20905634cd8857c0b988a2b49
Kye <71964154+kyelmao@users.noreply.github.com>
2021-05-28 13:57:15 +0100

    updated your main file :)

--- Bot/main.py
+++ Bot/main.py
@@ -4,104 +4,99 @@ import discord
 import asyncio
 import webserver
 from discord.ext import commands
+from discord.ext.commands import AutoShardedBot as asb
 from background_tasks import bg_tasks, spam_chart_daemon
 from utils import check_command, is_dev
 import importlib
 
+async def prefix(bot, message):
+    return "?" if message.guild.id == 437048931827056642 else "$"
 
-async def determine_prefix(bot, message):
-    if message.guild and message.guild.id == 437048931827056642:
-        return "$"
-    return "?"
+started = False
 
-bot = commands.Bot(
-    command_prefix=determine_prefix,
-    case_insensitive=True
-)
-
-
-# regex to find most print statements \n\s*print\([^()]*\)
-class initialized:
+class AllSeeingBot(asb):
     def __init__(self):
-        self.started = False
-
-    def __call__(self):
-        if not self.started:
-            self.started = True
-            return False
-        else:
-            return True
-
-
-started = initialized()
-
-extensions = [
-    'Commands.dev_cmds',
-    'Commands.data_tweaking',
-    # 'Other.Starboard.main',
-    'Commands.nicks',
-    'Commands.moderation',
-    'Moderation.main',
-    'Commands.fun',
-    'Commands.help',
-    # 'Mail.main',
-    'Other.logger'
-]
-
-converter = commands.RoleConverter()
-
-def testFunc(ctx: commands.Context) -> bool:
-    return ctx.guild.id == 437048931827056642
-
-@bot.command(name="helper")
-@commands.check(testFunc)
-async def toggle_helper_role(ctx: commands.Context, name: str):
-    try:
-        role = await converter.convert(ctx, f'help-{name}')
-    except commands.errors.BadArgument:
-        await ctx.send("nope nope doesnt exist nice try")
-        return
-    
-    if role in ctx.author.roles:
-        await ctx.author.remove_roles(role)
-    else:
-        await ctx.author.add_roles(role)
-    
-    await ctx.send('done')
-        
-
-
-if __name__ == '__main__':
-    for extension in extensions:
-        bot.load_extension(extension)
-
-
-@bot.event
-async def on_ready():
-    bot.add_check(check_command)
-    await bot.change_presence(
-        activity=discord.Activity(
-            name='everything', type=discord.ActivityType(3)
+        super().__init__(
+            command_prefix=self.prefix,
+            case_insensitive=True,
+            intents=discord.Intents.all(), # remove this if you dont have all intents enabled
+            help_command=None
         )
-    )
-    # print(len(bot.commands))
-    if not started():
-        bot.loop.create_task(spam_chart_daemon(bot))
-        bot.loop.create_task(bg_tasks(bot))
+        
+        self.cog_folders = [
+            "Commands",
+            "Moderation",
+            "Other"
+        ]
+        self.cog_blacklist = [
+            "__init__.py"
+        ]
+        
+        if not started:
+            print("Loading cogs:")
+            for file in os.listdir("."):
+                if os.path.isdir(f"./{file}") and file in self.cog_folders:
+                    for script in os.listdir(f"./{file}"):
+                        if script.endswith(".py") and not script in self.cog_blacklist:
+                            try:
+                                self.load_extension(f"{file}.{script[:-3]}")
+                                print(f"    Loaded '{script}'")
+                            except Exception as e:
+                                print(str(e))
+                        elif not "." in script and os.path.isdir(f"./{file}/{script}"):
+                            print(f"    Loading cogs from '{script}':")
+                            for script1 in os.listdir(f"./{file}/{script}"):
+                                try:
+                                    self.load_extension(f"{file}.{script}.{script1}")
+                                    print(f"    Loaded '{script1}'")
+                                except Exception as e:
+                                    print(str(e))
+            started = True
+                        
+    def testFunc(self, ctx: commands.Context) -> bool:
+        return ctx.guild.id == 437048931827056642
 
-    # print('ready')
+    @commands.command(name="helper")
+    @commands.check(testFunc)
+    async def toggle_helper_role(self, ctx: commands.Context, name: str):
+        try:
+            role = await converter.convert(ctx, f'help-{name}')
+        except commands.errors.BadArgument:
+            await ctx.send("nope nope doesnt exist nice try")
+            return
+        
+        if role in ctx.author.roles:
+            await ctx.author.remove_roles(role)
+        else:
+            await ctx.author.add_roles(role)
+        
+        await ctx.send('done')
 
+    async def on_ready():
+        bot.add_check(check_command)
+        await bot.change_presence(
+            activity=discord.Activity(
+                name='everything', type=discord.ActivityType(3)
+            )
+        )
+        # print(len(bot.commands))
+        if not started():
+            bot.loop.create_task(spam_chart_daemon(bot))
+            bot.loop.create_task(bg_tasks(bot))
 
-@bot.event
-async def on_command_error(ctx, error):
-    if type(error) == commands.errors.CheckFailure:
-        await ctx.message.delete()
-        msg = await ctx.send(f"You cant use `{ctx.prefix}{ctx.command}` here...")
-        await asyncio.sleep(3)
-        await msg.delete()
-    else:
-        raise error
+        # print('ready')
 
-webserver.keep_alive(bot)
-token = os.environ.get("DISCORD_BOT_SECRET")
-bot.run(token)
+    async def on_command_error(ctx, error):
+        if type(error) == commands.errors.CheckFailure:
+            await ctx.message.delete()
+            msg = await ctx.send(f"You cant use `{ctx.prefix}{ctx.command}` here...")
+            await asyncio.sleep(3)
+            await msg.delete()
+        else:
+            raise error
+
+if __name__ == "__main__":
+    webserver.keep_alive(bot)
+    token = os.environ.get("DISCORD_BOT_SECRET")
+    bot = AllSeeingBot()
+    bot.run(token)
